1d76f103178f6a8bfce32baae31a4abf46c46fad
Prodip Datta <prodipdatta7@gmail.com>
2018-12-31 23:59:11 +0600

    Add files via upload

---
+++ A.cpp
@@ -0,0 +1,115 @@
+/*...Part - 01...*/
+
+#include <iostream>
+#include <algorithm>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <string>
+#include <cmath>
+#include <vector>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <iterator>
+#include <bitset>
+#include <assert.h>
+#include <new>
+#include <sstream>
+//#include <bits/stdc++.h>
+using namespace std ;
+
+/*...Part - 02...*/
+
+typedef long long               ll ;
+typedef long double             ld ;
+typedef unsigned long long      ull ;
+typedef pair<int,int>           pii ;
+typedef pair<ll,ll>             pll ;
+typedef vector<int>             vi ;
+typedef vector<ll>              vll ;
+typedef vector<vector<int>>     vvi ;
+
+int Int(){int x ; scanf("%d",&x) ; return x ;}
+ll Long(){ll x ; scanf("%lld",&x) ; return x ;}
+
+/*...Part - 03...*/
+/*....Debugger....*/
+
+#define error(args...) { string _s = #args; replace(_s.begin(), _s.end(), ',', ' '); stringstream _ss(_s); istream_iterator<string> _it(_ss); err(_it, args); }
+void err(istream_iterator<string> it) {cout << endl ;}
+template<typename T, typename... Args>
+void err(istream_iterator<string> it, T a, Args... args) {
+    cerr << *it << " = " << a << ' ' ;
+    err(++it, args...);
+}
+
+/*...Part - 04...*/
+/*...Needed to change according to problem requirements...*/
+
+const int N          = (int)2e5 + 5 ;
+const int maxN       = (int)2e6 + 6 ;
+const ll  Mod        = (ll)1e9 + 7 ;
+const int inf        = (int)2e9 ;
+const ll  Inf        = (ll)1e18 ;
+
+/*..........................................................*/
+/*...Part - 05...*/
+
+#define     debug(x)    cerr << #x << " = " << x << '\n' ;
+#define     rep(i,b,e)  for(__typeof(e) i = (b) ; i != (e + 1) - 2 * ((b) > (e))  ; i += 1 - 2 * ((b) > (e)))
+#define     Int         Int()
+#define     Long        Long()
+#define     all(x)      x.begin() , x.end()
+#define     sz(x)       (int)x.size()
+#define     ff          first
+#define     ss          second
+#define     pb          push_back
+#define     eb          emplace_back
+#define     mem(a)      memset(a , 0 ,sizeof a)
+#define     memn(a)     memset(a , -1 ,sizeof a)
+
+/*...Part - 06...*/
+/*...... ! Code start from here ! ......*/
+
+ll modpow(ll b, ll p){
+    ll res = 1LL ;
+    while(p > 0){
+        if(p & 1)res = (res * b) % Mod ;
+        b = (b * b) % Mod ;
+        p >>= 1LL ;
+    }
+    return res % Mod ;
+}
+
+ll fact[maxN] ;
+
+void calc(){
+    fact[0] = 1LL ;
+    for(int i = 1 ; i < maxN ; ++i){
+        fact[i] = fact[i - 1] * i;
+        fact[i] %= Mod ;
+    }
+}
+
+int main(){
+    int test = Int , tc = 0 ;
+    calc() ;
+    while(test--){
+        int n = Int, k = Int ;
+        ll res = fact[n + k - 1] ;
+        res = res * modpow((fact[n]*fact[k - 1]) % Mod, Mod - 2) ;
+        res %= Mod ;
+        printf("Case %d: %lld\n",++tc, res);
+    }
+    return 0 ;
+}
+
+/*...Always look at the part - 04...*/
+/*...............END................*/
+
+
